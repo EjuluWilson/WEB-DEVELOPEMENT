ab6e7c39e870939a47e6d7a2dd7ac823f041bbff
Ejulu <emox1995@gmail.com>
2020-07-21 21:13:14 +0300

    added files

---
+++ APIs and Data/theCodingTrain/csvAndGraphing/chartify.js
@@ -0,0 +1,103 @@
+
+//import { ChartsModule } from ;
+//import  * as Chart from 'chart.js';
+//import Chart from './bower_components/chart.js/src/index.js';
+//C:\Users\Anne Grace Atim\Desktop\WEB DEVELOPEMENT\APIs and Data\theCodingTrain\csvAndGraphing\bower_components
+//import { Chart} from "./node_modules/chart.js/dist/Chart.js" 
+//import Chart from 'chart.js';
+// async function goChart(){
+    
+// }
+//var Chart = require("./bower_components/chart.js/src/index.js")
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+//chart 2
+import getData, {year,temp} from "./getData.js"
+
+
+async function plotTempData(){
+    await getData();
+
+    var ctx = document.getElementById('teperature').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        
+        data: {
+            labels: year,
+            datasets: [{
+                label: 'Annual Average Temperature',
+                data: temp,
+                fill:false,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+}
+
+plotTempData();
+
+
